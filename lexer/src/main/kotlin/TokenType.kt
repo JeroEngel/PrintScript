4d39b5fdf63d50d654e5c419ec3a61e0fdae1652
Federico <fkraft@mail.austral.edu.ar>
2024-08-13 19:18:03 -0300

    columns+row fix and lexer fix

--- lexer/src/main/kotlin/TokenType.kt
+++ lexer/src/main/kotlin/TokenType.kt
@@ -10,6 +10,8 @@ enum class TokenType {
 
     // IDENTIFIER
     IDENTIFIER,
+    STRING_TYPE,
+    NUMBER_TYPE,
 
     // OPERATORS
     ASSIGN,  // Renombrado de ASSIGNATION a ASSIGN para consistencia
