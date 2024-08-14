f2fa1f0618eec5dbd1f37c8ffa59165dc173e076
Your GitHub Username <your.email@example.com>
2024-08-13 18:26:26 -0700

    algunos changes en el parser

--- parser/src/main/kotlin/command/VariableDeclarationStatementCommand.kt
+++ parser/src/main/kotlin/command/VariableDeclarationStatementCommand.kt
@@ -4,18 +4,18 @@ import ASTNode
 import IdentifierNode
 import StringLiteralNode
 import Token
-import TokenType
 import VariableDeclarationNode
 
-class VariableDeclarationCommand : ParseCommand {
-    override fun execute(tokens: List<Token>, startIndex: Int): Pair<ASTNode, Int> {
-        val identifierToken = tokens[startIndex + 1]
-        val valueToken = tokens[startIndex + 3]
+class VariableDeclarationStatementCommand : ParseCommand {
+    override fun execute(tokens: List<Token>): ASTNode {
+        val identifierToken = tokens[1]
+        val valueToken = tokens[3]
 
         val identifierNode = IdentifierNode(identifierToken.value, identifierToken.line, identifierToken.column)
         val valueNode = StringLiteralNode(valueToken.value, valueToken.line, valueToken.column)
 
         val variableNode = VariableDeclarationNode(identifierNode, valueNode, identifierToken.line, identifierToken.column)
-        return Pair(variableNode, startIndex + 5)  // Ajusta el índice según la estructura de tokens
+
+        return variableNode
     }
 }
