a4ab8302f12aca1ed9f1d775b6608437306566ed
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-08-21 14:26:22 -0300

    cambios en ParserTest.kt, chequeo sintactico funcionando

--- parser/src/test/kotlin/ParserTest.kt
+++ parser/src/test/kotlin/ParserTest.kt
@@ -12,17 +12,17 @@ class ParserTests {
     @Test
     fun `test parser declaration with pre made list of tokens`() {
         val tokens = listOf(
-            Token(TokenType.LET, "let", 1, 1),
-            Token(TokenType.IDENTIFIER, "name", 1, 5),
-            Token(TokenType.COLON, ":", 1, 9),
-            Token(TokenType.STRING_TYPE, "String", 1, 11),
-            Token(TokenType.ASSIGN, "=", 1, 18),
-            Token(TokenType.STRING, "Olive", 1, 20),
-            Token(TokenType.SEMICOLON, ";", 1, 27),
-            Token(TokenType.IDENTIFIER, "name", 2, 1),
-            Token(TokenType.ASSIGN, "=", 2, 5),
-            Token(TokenType.STRING, "Juan", 2, 7),
-            Token(TokenType.SEMICOLON, ";", 2, 12),
+            Token(TokenType.LET, TokenValue.StringValue("let"), 1, 1),
+            Token(TokenType.IDENTIFIER,TokenValue.StringValue("name"), 1, 5),
+            Token(TokenType.COLON, TokenValue.StringValue(":"), 1, 9),
+            Token(TokenType.STRING_TYPE,TokenValue.StringValue("String"), 1, 11),
+            Token(TokenType.ASSIGN, TokenValue.StringValue("="), 1, 18),
+            Token(TokenType.STRING, TokenValue.StringValue("Olive"), 1, 20),
+            Token(TokenType.SEMICOLON, TokenValue.StringValue(";"), 1, 27),
+            Token(TokenType.IDENTIFIER, TokenValue.StringValue("name"), 2, 1),
+            Token(TokenType.ASSIGN, TokenValue.StringValue("="), 2, 5),
+            Token(TokenType.STRING, TokenValue.StringValue("Juan"), 2, 7),
+            Token(TokenType.SEMICOLON, TokenValue.StringValue(";"), 2, 12),
         )
 
         // Create a parser and parse the tokens
@@ -48,11 +48,11 @@ class ParserTests {
     @Test
     fun `test parser print statement with pre made list of tokens`() {
         val tokens = listOf(
-            Token(TokenType.PRINT, "print", 1, 1),
-            Token(TokenType.LEFT_PARENTHESIS, "(", 1, 6),
-            Token(TokenType.STRING, "Olive", 1, 7),
-            Token(TokenType.RIGHT_PARENTHESIS, ")", 1, 13),
-            Token(TokenType.SEMICOLON, ";", 1, 14),
+            Token(TokenType.PRINT, TokenValue.StringValue("print"), 1, 1),
+            Token(TokenType.LEFT_PARENTHESIS, TokenValue.StringValue("("), 1, 6),
+            Token(TokenType.STRING, TokenValue.StringValue("Olive"), 1, 7),
+            Token(TokenType.RIGHT_PARENTHESIS, TokenValue.StringValue(")"), 1, 13),
+            Token(TokenType.SEMICOLON, TokenValue.StringValue(";"), 1, 14),
 
         )
 
@@ -73,17 +73,17 @@ class ParserTests {
         // Assert that the AST produced by the parser matches the expected AST
         assertEquals(expectedAst, ast)
     }
+
     @Test
     fun `test parser print statement with pre made list of tokens part 2`() {
         val tokens = listOf(
-            Token(TokenType.PRINT, "print", 1, 1),
-            Token(TokenType.LEFT_PARENTHESIS, "(", 1, 6),
-            Token(TokenType.STRING, "5", 1, 7),
-            Token(TokenType.SUM, "+", 1, 13),
-            Token(TokenType.STRING, "6", 8, 7),
-            Token(TokenType.RIGHT_PARENTHESIS, ")", 1, 13),
-            Token(TokenType.SEMICOLON, ";", 1, 14),
-
+            Token(TokenType.PRINT, TokenValue.StringValue("print"), 1, 1),
+            Token(TokenType.LEFT_PARENTHESIS, TokenValue.StringValue("("), 1, 6),
+            Token(TokenType.NUMBER, TokenValue.NumberValue(5.0), 1, 7),
+            Token(TokenType.SUM, TokenValue.StringValue("+"), 1, 13),
+            Token(TokenType.NUMBER, TokenValue.NumberValue(6.0), 8, 7),
+            Token(TokenType.RIGHT_PARENTHESIS, TokenValue.StringValue(")"), 1, 13),
+            Token(TokenType.SEMICOLON, TokenValue.StringValue(";"), 1, 14),
             )
 
         // Create a parser and parse the tokens
