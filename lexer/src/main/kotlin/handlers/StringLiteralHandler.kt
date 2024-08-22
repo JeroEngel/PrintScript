58a6e8968ae56a83d31c4ae9af4f39323970093b
Federico <fkraft@mail.austral.edu.ar>
2024-08-22 17:45:00 -0300

    added doubles handler and added "" or '' handler

--- lexer/src/main/kotlin/handlers/StringLiteralHandler.kt
+++ lexer/src/main/kotlin/handlers/StringLiteralHandler.kt
@@ -1,4 +1,3 @@
-
 package handlers
 
 import Lexer
@@ -9,19 +8,27 @@ import TokenValue
 
 class StringLiteralHandler : TokenHandler {
     override fun handle(currentChar: Char, lexer: Lexer): Token? {
-        if (currentChar == '"') {
+        if (currentChar == '"' || currentChar == '\'') {
+            val startChar = currentChar
             val start = lexer.position
             lexer.position++
             lexer.column++
-            while (lexer.position < lexer.code.length && lexer.code[lexer.position] != '"') {
+
+            while (lexer.position < lexer.code.length && lexer.code[lexer.position] != startChar) {
                 lexer.position++
                 lexer.column++
             }
-            lexer.position++
-            lexer.column++
-            val value = lexer.code.substring(start + 1, lexer.position - 1)
 
-            return Token(TokenType.STRING, TokenValue.StringValue(value), lexer.line, lexer.column - value.length - 2)
+            // Check if the closing character matches the opening character
+            if (lexer.position < lexer.code.length && lexer.code[lexer.position] == startChar) {
+                lexer.position++
+                lexer.column++
+                val value = lexer.code.substring(start + 1, lexer.position - 1)
+                return Token(TokenType.STRING, TokenValue.StringValue(value), lexer.line, lexer.column - value.length - 2)
+            } else {
+                // Handle mismatched string delimiters
+                throw IllegalArgumentException("Mismatched string delimiters: started with $startChar but didn't close properly.")
+            }
         }
         return null
     }
